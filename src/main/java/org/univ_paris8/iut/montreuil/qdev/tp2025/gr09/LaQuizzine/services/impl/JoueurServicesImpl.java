bd65d2bb8f80042b8fc3d513787f738367c470c0
Regan SASIKUMAR <rsasikumar@a205-02.iut.univ-paris8.fr>
2025-03-13 17:07:05 +0100

    fix sorties methode ajouterjoueur

--- src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr09/LaQuizzine/services/impl/JoueurServicesImpl.java
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr09/LaQuizzine/services/impl/JoueurServicesImpl.java
@@ -1,11 +1,11 @@
 package org.univ_paris8.iut.montreuil.qdev.tp2025.gr09.LaQuizzine.services.impl;
 
-import org.univ_paris8.iut.montreuil.qdev.tp2025.gr09.LaQuizzine.dto.JoueurDTO;
+import org.univ_paris8.iut.montreuil.qdev.tp2025.gr09.LaQuizzine.dto.ReponseAjouterJoueurDTO;
 import org.univ_paris8.iut.montreuil.qdev.tp2025.gr09.LaQuizzine.services.interfaces.IJoueurServices;
 
 public class JoueurServicesImpl implements IJoueurServices {
     @Override
-    public JoueurDTO ajouterJoueur(String prenom, String pseudo, int annee_naissance, String centre_interets, int langue) {
+    public ReponseAjouterJoueurDTO ajouterJoueur(String prenom, String pseudo, int annee_naissance, String centre_interets, int langue) {
         return null;
     }
 }
--- src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr09/LaQuizzine/services/interfaces/IJoueurServices.java
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr09/LaQuizzine/services/interfaces/IJoueurServices.java
@@ -1,7 +1,8 @@
 package org.univ_paris8.iut.montreuil.qdev.tp2025.gr09.LaQuizzine.services.interfaces;
 
 import org.univ_paris8.iut.montreuil.qdev.tp2025.gr09.LaQuizzine.dto.JoueurDTO;
+import org.univ_paris8.iut.montreuil.qdev.tp2025.gr09.LaQuizzine.dto.ReponseAjouterJoueurDTO;
 
 public interface IJoueurServices {
-    JoueurDTO ajouterJoueur(String prenom, String pseudo, int annee_naissance, String centre_interets, int langue);
+    ReponseAjouterJoueurDTO ajouterJoueur(String prenom, String pseudo, int annee_naissance, String centre_interets, int langue);
 }
