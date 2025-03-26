126cdf3a858b70cd8b78429edb1c7a266f6eb794
Regan SASIKUMAR <rsasikumar@a101-22.iut.univ-paris8.fr>
2025-03-26 12:30:12 +0100

    maj de LangueEnum

--- src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr09/LaQuizzine/utils/enums/LangueEnum.java
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr09/LaQuizzine/utils/enums/LangueEnum.java
@@ -1,9 +1,36 @@
 package org.univ_paris8.iut.montreuil.qdev.tp2025.gr09.LaQuizzine.utils.enums;
 
 public enum LangueEnum {
-    FRANCAIS,
-    ENGLISH,
-    DEUTCH,
-    ESPANA,
-    ITALIA
+    FRANCAIS(1, "Français"),
+    ANGLAIS(2, "Anglais"),
+    ESPAGNOL(3, "Espagnol"),
+    ALLEMAND(4, "Allemand"),
+    ITALIEN(5, "Italien");
+
+    private final int numero;
+    private final String nom;
+
+    LangueEnum(int numero, String nom) {
+        this.numero = numero;
+        this.nom = nom;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public static LangueEnum fromNumero(int numero) {
+        for (LangueEnum l : LangueEnum.values()) {
+            if (l.getNumero() == numero) {
+                return l;
+            }
+        }
+        return null;
+    }
+
+
 }
